2e69390c011d6f65387135beb45614b0d8cc245f
Eddie Chung <designered@gmail.com>
2019-12-08 18:00:49 +0100

    async redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,69 +1,71 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as courseActions from '../../redux/actions/courseActions';
+import { loadCourses } from '../../redux/actions/courseActions';
+import { loadAuthors } from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
+import { Redirect } from 'react-router-dom';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ''
+  state = { redirectToAddCoursePage: false };
+
+  componentDidMount() {
+    const { courses, authors, loadAuthors, loadCourses } = this.props;
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert('Loading courses failed' + error);
+      });
     }
-  };
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-    this.setState({
-      course: {
-        title: ''
-      }
-    });
-  };
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert('Loading authors failed' + error);
+      });
+    }
+  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
+        {this.state.redirectToAddCoursePage && <Redirect to='/course' />}
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type='text'
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-
-        <input type='submit' value='Save' />
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
-      </form>
+        <button
+          style={{ marginBottom: 20 }}
+          className='btn btn-primary add-course'
+          onClick={() => this.setState({ redirectToAddCoursePage: true })}
+        >
+          Add Course
+        </button>
+        <CourseList courses={this.props.courses} />
+      </>
     );
   }
 }
 
 CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    courses: state.courses
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
+    authors: state.authors
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(courseActions, dispatch)
-  };
-}
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducers';
 
 const rootReducer = combineReducers({
-  courses
+  courses,
+  authors
 });
 
 export default rootReducer;
